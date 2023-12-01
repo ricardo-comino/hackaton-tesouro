21bd3bfc2c778cc2a3281190299ff209e1eaf05b
DEAL\diego.costa <diego.costa@deal.com.br>
2023-12-01 10:52:33 -0300

    criação da rota

--- web-front/src/app/app-routing.module.ts
+++ web-front/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+// Adicione no app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SelecaoGarantiaComponent } from './selecao-garantia/selecao-garantia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/selecao-garantia', pathMatch: 'full' },
+  { path: 'selecao-garantia', component: SelecaoGarantiaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
